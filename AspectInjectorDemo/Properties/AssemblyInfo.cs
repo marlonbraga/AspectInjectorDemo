cdd02b1a4ad9e50cca563bda7e29316614a038bd
Marlon Braga <marlon@marlonbraga.com.br>
2024-11-24 23:01:44 -0300

    Create Readme

--- AspectInjectorDemo/Properties/AssemblyInfo.cs
+++ AspectInjectorDemo/Properties/AssemblyInfo.cs
@@ -1,10 +1,3 @@
-﻿using System;
-using System.Reflection;
-using AspectInjectorDemo;
+﻿using AspectInjectorDemo;
 
 [assembly: AutoLog]
-
-// You can also include other assembly-level attributes here, for example:
-//[assembly: AssemblyTitle("My Project Title")]
-//[assembly: AssemblyDescription("This is a project description")]
-//[assembly: AssemblyVersion("1.0.0.0")]
